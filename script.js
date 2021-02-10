44d70e0f1e2cd976e8c394fe4ffc8d8719d3fbb8
atanbashi <radek_10_86@mail.ru>
2021-02-10 21:14:51 +0300

    script

---
+++ script.js
@@ -0,0 +1,15 @@
+// const menu = document.querySelector('.menu');
+//
+// let button = document.querySelector('.button');
+//
+// let radio = document.querySelector('.radio__input:checked');
+//
+// button.onclick = function selectLevel() {
+//   menu.style.display = 'none';
+//   const three = document.querySelector('three');
+//   if (radio === true) {
+//     three.style.display = 'block';
+//   } else {
+//     return
+//   }
+// }
